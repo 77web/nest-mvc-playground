dc37385a2792c6baab08300101a1bba282ba6402
77web <info@77-web.com>
2021-08-19 01:02:36 +0900

    controllerでDBデータを取得してみる

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { EntityManager } from '@mikro-orm/core';
+import { User } from '../entity/user';
 
 @Controller()
 export class HomeController {
+  constructor(private readonly em: EntityManager) {}
+
   @Get()
   @Render('home/index')
-  index() {
-    return { hello: 'Hello World!' };
+  async index() {
+    const users = await this.em.find(User, {});
+    if (users.length === 0) {
+      return { hello: 'Hello Jane Doe!' };
+    }
+
+    return { hello: 'Hello ' + users[0].name };
   }
 }
